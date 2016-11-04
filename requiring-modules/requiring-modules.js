855ea3a1183420e027a376c59c2db5fd40e98761
Adam Dipinto <adam.dipinto@gmail.com>
2016-11-04 18:27:51 +0000

    Creating folder and exercise for requiring module

---
+++ requiring-modules/requiring-modules.js
@@ -0,0 +1,10 @@
+// Lighthouse Labs - W1D5 - Requiring Modules //
+
+
+module.exports = {
+  var random = Math.floor(Math.random()*100);
+
+  explain: function() {
+    console.log("A random number between 1 and 100: " + this.random);
+  }
+};
