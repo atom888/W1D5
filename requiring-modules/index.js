2101290eea21ba0c21b84683e90e2e4533985fc1
Adam Dipinto <adam.dipinto@gmail.com>
2016-11-04 19:26:49 +0000

    Adding index.js file - requires function from requiring modules file.

---
+++ requiring-modules/index.js
@@ -0,0 +1,7 @@
+// Lighthouse Labs W1D5 Exercise Requiring Modules //
+
+var temp = require("./requiring-modules");
+
+
+console.log(temp.randMath());
+
--- requiring-modules/requiring-modules.js
+++ requiring-modules/requiring-modules.js
@@ -1,10 +1,26 @@
 // Lighthouse Labs - W1D5 - Requiring Modules //
 
 
-module.exports = {
-  var random = Math.floor(Math.random()*100);
 
-  explain: function() {
-    console.log("A random number between 1 and 100: " + this.random);
+  function randMath() {
+    return funTimes();
   }
-};
+
+  function funTimes() {
+    var randNum = Math.floor(Math.random()*100);
+    return "Here is a random number: " + randNum;
+  }
+
+module.exports = {randMath};
+
+
+// module.exports = {
+//   randomNum = Math.floor(Math.random()*100);
+//   explain: function () {
+//     console.log(this.randomNum + " was a random number selected between 1 and 100.");
+
+//   funTimes = "Have a good day."
+
+// }
+
+
